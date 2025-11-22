7b0716a93f7c6c29b2ad67698200934d63b08938
Wilbert Baker <Wilb1841@gmail.com>
2025-11-22 15:17:45 -0500

    Initial Commit

---
+++ teacup-coffee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  teacup-coffee
+//
+//  Created by Wilbert Baker on 11/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ teacup-coffee/teacup_coffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  teacup_coffeeApp.swift
+//  teacup-coffee
+//
+//  Created by Wilbert Baker on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct teacup_coffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
